b08be935e3ef20c13f3bcf189f3e43687ec61614
unknown <ssidd021@uottawa.ca>
2022-03-10 09:18:19 -0500

    added pickle result files, and randomforest training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,10 +6,12 @@ import os.path
 import os
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 from sklearn.neural_network import MLPClassifier
+import pickle
 
 
 def svm(X_train, X_test, y_train, y_test, to_save_dir):
@@ -27,6 +29,29 @@ def svm(X_train, X_test, y_train, y_test, to_save_dir):
     df = pd.DataFrame(report).transpose()
     df.to_csv(os.path.join(to_save_dir, 'svm.csv'))
 
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'svm.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
+
+
+def random_forest(X_train, X_test, y_train, y_test, to_save_dir):
+    clf = RandomForestClassifier(random_state=314)
+    clf.fit(X_train, y_train)
+    predicted_y = clf.predict(X_test)
+    tn, fp, fn, tp = confusion_matrix(y_test, predicted_y).ravel()
+    precision_score = tp / (tp + fp)
+    recall_score = tp / (tp + fn)
+    print(metrics.confusion_matrix(y_test, predicted_y))
+    print(metrics.classification_report(y_test, predicted_y))
+
+    report = metrics.classification_report(y_test, predicted_y, output_dict=True)
+    df = pd.DataFrame(report).transpose()
+    df.to_csv(os.path.join(to_save_dir, 'random_forest.csv'))
+
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'random_forest.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
+
 
 def decision_tree(X_train, X_test, y_train, y_test, to_save_dir):
     clf = tree.DecisionTreeClassifier()
@@ -43,6 +68,10 @@ def decision_tree(X_train, X_test, y_train, y_test, to_save_dir):
     df = pd.DataFrame(report).transpose()
     df.to_csv(os.path.join(to_save_dir, 'decision_tree.csv'))
 
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'decision_tree.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
+
 
 def kNN(X_train, X_test, y_train, y_test, to_save_dir):
     clf = KNeighborsClassifier(n_neighbors=5)
@@ -55,6 +84,10 @@ def kNN(X_train, X_test, y_train, y_test, to_save_dir):
     report = metrics.classification_report(y_test, predicted_y, output_dict=True)
     df = pd.DataFrame(report).transpose()
     df.to_csv(os.path.join(to_save_dir, 'kNN.csv'))
+
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'kNN.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
 
 
 def gradient_boost(X_train, X_test, y_train, y_test, to_save_dir):
@@ -72,6 +105,10 @@ def gradient_boost(X_train, X_test, y_train, y_test, to_save_dir):
     df = pd.DataFrame(report).transpose()
     df.to_csv(os.path.join(to_save_dir, 'gradient_boost.csv'))
 
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'gradient_boost.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
+
 
 def neural_net_mlp(X_train, X_test, y_train, y_test, to_save_dir):
     clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(14, 10 ), random_state=1, max_iter=300, warm_start=True)
@@ -87,6 +124,10 @@ def neural_net_mlp(X_train, X_test, y_train, y_test, to_save_dir):
     report = metrics.classification_report(y_test, predicted_y, output_dict=True)
     df = pd.DataFrame(report).transpose()
     df.to_csv(os.path.join(to_save_dir, 'mlp.csv'))
+
+    pickle_dir = to_save_dir + "pickle_results\\"
+    with open(os.path.join(pickle_dir, 'mlp.pkl'), 'wb') as f:
+        pickle.dump(clf, f)
 
 
 def encode_string_values(df):
@@ -111,8 +152,11 @@ def main():
         frames.append(pd.read_csv(attack_path))
 
     result = pd.concat(frames)  # Combine all dataframes
-    result = result.fillna(0)  # Fill in all NA values
     result = encode_string_values(result)  # Encode non float values
+    result = result.drop('Hostname', 1)
+    result = result.drop('SourceHostname', 1)
+    result = result.drop('DestinationHostname', 1)
+    result = result.fillna(0)  # Fill in all NA values
 
     X_train, X_test, y_train, y_test = train_test_split(result.drop('is_attack', 1), result['is_attack'],
                                                         test_size=0.25, random_state=42, shuffle=True)
@@ -125,6 +169,7 @@ def main():
     decision_tree(X_train, X_test, y_train, y_test, models_dir)
     gradient_boost(X_train, X_test, y_train, y_test, models_dir)
     neural_net_mlp(X_train, X_test, y_train, y_test, models_dir)
+    random_forest(X_train, X_test, y_train, y_test, models_dir)
 
 
 if __name__ == '__main__':
